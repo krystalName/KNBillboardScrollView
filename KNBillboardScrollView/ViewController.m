eb128d43a4bd0b74fd4584e04a5d5d388dbc2a3b
Leessang <992275294@qq.com>
2018-03-08 14:27:57 +0800

    修改代码

--- KNBillboardScrollView/ViewController.m
+++ KNBillboardScrollView/ViewController.m
@@ -24,9 +24,27 @@
     [super viewDidLoad];
 
     
+    NSArray *imageArray = @[
+                            [YYImage imageNamed:@"3.jpg"],
+                            @"http://pic1.win4000.com/wallpaper/2017-11-15/5a0bac8b71131.jpg",
+                            //使用本地gif图片的时候。需要调用这个方法
+                            [YYImage imageNamed:@"2.gif"],
+                            @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510747412310&di=308b1b2e2d6ccb6a35796275fc185eaf&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f90e593a4ad4a8012193a3dcf03d.gif"
+                            ];
     
-    //创建广告栏
-    _bollboadrView = [[KNBillboadrView alloc]initKNBillboadrViewWithFrame:CGRectMake(0, 50, [UIScreen mainScreen].bounds.size.width, 200) andplaceholdImage:[UIImage imageNamed:@"KNBilboardDefalutImge.png"]];
+    NSArray *descArray = @[@"本地看星星",@"网络图片",@"本地gif图片",@"网络gif图片"];
+    
+    //第一种方式创建广告栏。图片数据已经存在的情况下
+    _bollboadrView = [[KNBillboadrView alloc]initKNBillboadrViewWithFrame:CGRectMake(0, 50, [UIScreen mainScreen].bounds.size.width, 200) andImageArray:imageArray andDescArray:descArray andplaceholdImage:[UIImage imageNamed:@"KNBilboardDefalutImge.png"]];
+    
+    
+    //第二种方式创建广告栏。 图片数据暂时没有的情况下
+  
+    /** _bollboadrView = [[KNBillboadrView alloc]initKNBillboadrViewWithFrame:CGRectMake(0, 50, [UIScreen mainScreen].bounds.size.width, 200) andplaceholdImage:[UIImage imageNamed:@"KNBilboardDefalutImge.png"]];
+     */
+    
+    
+    
     [_bollboadrView setBackgroundColor:[UIColor grayColor]];
     
     //设置代理
@@ -75,7 +93,9 @@
         [_LoadDataButton setTitle:@"加载数据" forState:(UIControlStateNormal)];
         _LoadDataButton.titleLabel.font = [UIFont systemFontOfSize:13];
         [_LoadDataButton setBackgroundColor:[UIColor redColor]];
+        /*
         [_LoadDataButton addTarget:self action:@selector(ClickLoad:) forControlEvents:UIControlEventTouchUpInside];
+         */
     }
     return _LoadDataButton;
 }
